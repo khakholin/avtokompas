d16e3a423b2099b4569e4a3da03cb6ff49e6cd0e
Alexander Khakholin <54610566+khakholin@users.noreply.github.com>
2019-10-10 15:57:25 +0700

    Upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function showMenu() {
+    let menu = document.getElementsByClassName('menu')[0];
+    if (menu.className == 'menu') {
+        menu.className = 'menu menu_active';
+    }
+    else{
+        menu.className = 'menu';
+    }
+}
