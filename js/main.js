dd9632f5b3a77b7fc7c7c1273791048fbdd62a0b
khakholin <khakholin@mail.ru>
2019-10-16 15:43:45 +0700

    interactive with js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,35 @@
-let menuButton = document.getElementsByClassName('menu-button')[0];
-menuButton.onclick = function() {
-    let menu = document.getElementsByClassName('menu')[0];
-
-    if (menu.className == 'menu') {
-        menu.className = 'menu menu_active';
-    }
-    else{
-        menu.className = 'menu'
-    }
-}
+$(window).load(function(){
+    $(".before-after").twentytwenty({
+        before_label: 'С Автокомпасом',
+        after_label: 'Без Автокомпаса'
+    });
+    $('.before-slider').slick({
+        draggable: false,
+        dots: true,
+        dotsClass: 'before-slider__dots',
+        prevArrow: $('.arrow-left'),
+        nextArrow: $('.arrow-right'),
+    });
+    $('.menu-button').on('click', function(){
+        $('.menu').toggleClass('menu_active');
+        $('.menu-button__lines').toggleClass('menu-button__lines_active');
+    });
+    $('.select_checked').on('click', function(){
+        $('.select__dropdown').toggleClass('select__dropdown_open');
+    });
+    $('.select__option').on('click', function(){  
+        var value = $(this).attr('data-value');
+        $('#select-type').val(value);
+        $('.select_checked').text(value);
+        $('.select__dropdown').toggleClass('select__dropdown_open');
+    });
+    $("a[href^='#']").click(function(){
+        var _href = $(this).attr("href");
+        $("html, body").animate({ scrollTop: $(_href).offset().top -150 + "px"});
+        return false;
+    });
+    $('input[type["tel"]').mask("+7 (999) 999-99-99");
+});
 for (let i = 0; i < 6; i++) {
     let imageBlock = document.getElementsByClassName('price-card__image')[i];
     let curImage = imageBlock.getElementsByTagName('img')[0];
