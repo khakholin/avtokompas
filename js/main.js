84aae18690444bd8598de696501cfca8423db53c
khakholin <khakholin@mail.ru>
2019-10-17 15:25:18 +0700

    reviews block adaptive and change of design

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ $(window).load(function(){
         dots: true,
         dotsClass: 'before-slider__dots',
         prevArrow: $('.arrow-left'),
-        nextArrow: $('.arrow-right'),
+        nextArrow: $('.arrow-right')
     });
     $('.menu-button').on('click', function(){
         $('.menu').toggleClass('menu_active');
@@ -28,19 +28,28 @@ $(window).load(function(){
         $("html, body").animate({ scrollTop: $(_href).offset().top -150 + "px"});
         return false;
     });
-    $('input[type["tel"]').mask("+7 (999) 999-99-99");
+    $('input[type="tel"]').mask("+7 (999) 999-99-99");
+    /*Отображение карты в момент докрутки*/
+    var reviews = $('.reviews');
+    var reviewsTop = reviews.offset().top;
+    $(window).bind('scroll', function() {
+        var windowTop = $(this).scrollTop();
+        if (windowTop > reviewsTop) {
+            $('#map').html('<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A89090f0b70c4125f48bb99429c8745a495cd5abb4cf5c6dcc789494dcbfaaccb&amp;width=100%25&amp;height=410&amp;lang=ru_UA&amp;scroll=false"></script>')
+            $(window).unbind('scroll');
+        }
+    });
 });
 for (let i = 0; i < 6; i++) {
-    let imageBlock = document.getElementsByClassName('price-card__image')[i];
+    let imageBlock = document.getElementsByClassName('car-park-card__image')[i];
     let curImage = imageBlock.getElementsByTagName('img')[0];
     imageBlock.onmouseover = imageBlock.onmouseout = handler;
-
     function handler(event) {
         if (event.type == 'mouseover') {
-            curImage.src = 'img/price/'+ (i+1) + '.1.jpg';
+            curImage.src = 'img/car-park/'+ (i+1) + '.1.jpg';
         }
         if (event.type == 'mouseout') {
-            curImage.src = 'img/price/'+ (i+1) + '.jpg';
+            curImage.src = 'img/car-park/'+ (i+1) + '.jpg';
         }
     }
 }
